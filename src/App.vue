deb919c6e3777f5e7292fe2e68959ec48ae42fbd
Divine Eze <divinee379@gmail.com>
2026-07-25 23:34:48 +0100

    making game catalogue front 9

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+    components: {
+            NavBar
+        },
+        
+    data() {
+        return {      
+        
+    }
+    }
+}
+</script>
+
+<template>
+ <NavBar />
+</template>
+
+<style >
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script>
+import logo from "/Front9-text-only.png"
+export default{
+    data(){
+        return {
+            img: logo
+        }
+    }
+}
+</script>
+<template>
+    <div class="flex">
+        <img :src="img" alt="front-9-logo">
+        <input type="search" class="border-2">
+        <p>Login</p>
+        <p>Sign Up</p>
+    </div>
+</template>
