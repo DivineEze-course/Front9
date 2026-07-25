2d1d48a8d7c2683b031b49a5179e852a325a65a9
Divine Eze <divinee379@gmail.com>
2026-07-26 00:02:31 +0100

    made navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <script>
-import logo from "/Front9-text-only.png"
+import logo from "../assets/images/logo/Front9-text-only.png"
 export default{
     data(){
         return {
@@ -9,10 +9,14 @@ export default{
 }
 </script>
 <template>
-    <div class="flex">
-        <img :src="img" alt="front-9-logo">
-        <input type="search" class="border-2">
+    <div class="flex p-10 bg-white gap-15 justify-center items-center">
+        <div>
+        <img :src="img" alt="front-9-logo" class="w-[150px]">
+        </div>
+        <input type="search" placeholder="Search games" class="w-[760px] border-1 border-gray-200 rounded-lg bg-gray-200 p-2 hover:border-[rgb(46,189,182)] hover:border-2 focus:outline-[rgb(46,189,182)] focus:bg-white-">
+        <div class=" flex font-bold text-[rgb(46,189,182)] gap-10 text-lg">
         <p>Login</p>
         <p>Sign Up</p>
+        </div>
     </div>
 </template>
